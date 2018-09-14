3a6196c55f0419175d543f8317933976b937585e
juank8911 <juancarlos_891111@hotmail.com>
2018-09-14 16:04:04 -0500

    arreglo de horas para citas

--- src/routes/eventsRoutes.js
+++ src/routes/eventsRoutes.js
@@ -11,14 +11,28 @@ module.exports = function(app)
 
 
 app.post('/events',(req,res)=>{
+  var Mend = parseInt(req.body.end);
+  var hinicio = moment(req.body.start).format('HH:mm:ss');
+  var Finicio = moment(req.body.start).format('YYYY-MM-DD');
+  var horas = hinicio.split(":");
+  var mins = horas[1];
+  var hora = horas[0];
+  hora = parseInt(hora)+5;
+  mins = parseInt(mins);
+  minsEnd = mins+Mend;
+  var Hstart = hora+":"+"00"+":00";
+  var Hend = hora+":"+minsEnd+":00";
+  var starts = Finicio+" "+Hstart;
+  var ends = Finicio+" "+Hend;
+  //var Hend = moment(ends).format('YYYY-MM-D HH:mm:ss');
   var eventss = {
     color: req.body.color,
-    start: req.body.start,
-    end: req.body.end,
+    start: starts,
+    end: ends,
     usuario: req.body.id_usuario,
     servicio: req.body.id_servicio
   };
-  console.log(eventss);
+  console.log(starts + " "+ends);
   events.agregarEvento(eventss,(err,data)=>{
     res.json(data);
   });
