c13ff98abe29666e406d41262669e546181529ad
juank8911 <juancarlos_891111@hotmail.com>
2018-09-19 16:43:48 -0500

    derrotero de horas completo

--- src/routes/horarioRoutes.js
+++ src/routes/horarioRoutes.js
@@ -4,11 +4,21 @@ module.exports=function(app)
 {
   app.post('/horario',(req, res)=>{
     horario = req.body.dias;
-    horario.id=4;
+    horario.id=2;
       console.log(horario);
     hora.agregarHorario(horario,(err,resp)=>{
       res.json(resp);
     })
+});
+
+app.post('/citas',(req,res)=>{
+  serv = {
+    fecha:req.body.fecha,
+    id:req.body.id
+  };
+  hora.darDia(serv,(err,resp)=>{
+    res.json(resp);
+  });
 });
 
 }
