9764621a47c69f892b0e451dffda3115cac0612d
juank8911 <juancarlos_891111@hotmail.com>
2018-09-20 09:48:25 -0500

    cambio de post

--- src/models/horario.js
+++ src/models/horario.js
@@ -19,7 +19,7 @@ horarioModel.agregarHorario = (horario,callback) =>
   {
     var hsql = 'INSERT INTO horario (de_maniana,a_maniana,de_tarde,a_tarde,servicios_id_servicios) VALUES (?,?,?,?,?)';
     var sql='INSERT INTO semana(lunes,martes,miercoles,jueves,viernes,sabado,domingo,horario_idhorario) VALUES (?,?,?,?,?,?,?,?)';
-      connection.query(hsql,[horas.m_de,horas.m_hasta,horas.t_de,horas.t_hasta,horario.id],(err,row)=>{
+      connection.query(hsql,[horario.m_de,horario.m_hasta,horario.t_de,horario.t_hasta,horario.id],(err,row)=>{
         if(err)
         {
           throw err
