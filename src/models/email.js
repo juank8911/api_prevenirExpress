ef24e73f20658b90c1d4c6c0e0912af5fc5c6237
juank8911 <juancarlos_891111@hotmail.com>
2018-10-03 10:10:21 -0500

    sss

--- src/models/email.js
+++ src/models/email.js
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 let mysql = require('mysql');
 let config = require('../config');
 var nodemailer = require('nodemailer');
+
 
 connection = mysql.createConnection({
 host: config.domain,
@@ -19,52 +19,7 @@ emailModel.sendMail = (mail,callback)=>{
     service: config.mailserv,
     auth: {
         user: config.userMail,
-        pass: config.passMail
-          }
-          });
-console.log(mail);
-
-  var mailOptions = {
-              from: mail.remitente,
-              to: mail.destino,
-              subject: mail.asunto,
-              text: mail.texto
-       }
-
-  transporter.sendMail(mailOptions, function(error, info){
-      if (error){
-          //console.log(error);
-        callback(null,'not send');
-      } else {
-          console.log("Email sent");
-          callback(null,'send');
-      }
-  });
-
-};
-module.exports = emailModel;
-=======
-let mysql = require('mysql');
-let config = require('../config');
-var nodemailer = require('nodemailer');
-
-connection = mysql.createConnection({
-host: config.domain,
-user: config.userbd,
-password: config.passwordbd,
-database: config.nombredb
-});
-
-
-let emailModel = {};
-
-emailModel.sendMail = (mail,callback)=>{
-
-  transporter = nodemailer.createTransport({
-    service: config.mailserv,
-    auth: {
-        user: config.userMail,
-        pass: config.passMail
+        pass: config.passMailgit
           }
           });
 console.log(mail);
@@ -88,4 +43,3 @@ console.log(mail);
 
 };
 module.exports = emailModel;
->>>>>>> 46f9862065a53ceec2545699cc4fdd3cd7cf3cf5
