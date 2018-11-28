db4385fc7d4c4b49bdef562407934afdd8780555
juank8911 <juancarlos_891111@hotmail.com>
2018-11-28 10:57:02 -0500

    cambios

--- src/models/citasinternas.js
+++ src/models/citasinternas.js
@@ -13,18 +13,46 @@ let citasIModule = {};
 
 //agrga una nueva cita para el servicio agragando o no un nuevo usuario
 citasIModule.nuevaCita = (cita,callback)=>{
+  if(connection)
+  {
+    if(cita.existe == true)
+    {
+
+    }
+    else
+    {
+
+    }
+
+  }
 
 };
 
 citasIModule.darUsuarios = (callback)=>{
   if(connection)
   {
-    var sel = 'SELECT cedula FROM usuarios'
+    var sel = 'SELECT cedula, nombre FROM usuarios'
     connection.query(sel,(err,row)=>{
       if(err){throw err}
       else
       {
         // console.log(row);
+        callback(null,row);
+      }
+    });
+  }
+};
+
+citasIModule.darUsuariosID = (id,callback)=>{
+  let ids = parseInt(id);
+  if(connection)
+  {
+    var sel = "SELECT cedula, nombre FROM usuarios WHERE cedula like '?%' ";
+    connection.query(sel,ids,(err,row)=>{
+      if(err){throw err}
+      else
+      {
+        console.log(row[0]);
         callback(null,row);
       }
     });
--- src/routes/citasIn.js
+++ src/routes/citasIn.js
@@ -4,13 +4,20 @@ module.exports = function (app)
 {
 
 //devuelve listado de categorias
+app.get('/cedula/:id',(req,res)=>{
+  let id = req.params.id;
+  console.log('da cedulas id');
+internas.darUsuariosID(id,(err,data)=>{
+res.json(data);
+});
+});
+
 app.get('/cedula',(req,res)=>{
   console.log('da cedulas');
 internas.darUsuarios((err,data)=>{
 res.json(data);
 });
 });
-
 
 
 }
