8b3797048edc3e85e798fc966ff8fc967810d2e4
Prevenir-Desarrollo <prevenir@iMac-de-Prevenir-Desarrollo.local>
2019-06-05 11:22:48 -0500

    cracion de modulo de oiptica

---
+++ src/models/optica.js
@@ -0,0 +1,23 @@
+let mysql = require('mysql');
+let config = require('../config');
+
+connection = mysql.createConnection({
+host: config.domain,
+user: config.userbd,
+password: config.passwordbd,
+database: config.nombredb
+});
+
+let optModule = {};
+
+optModule.darDatosOptUsu = (callback) => {
+var sql = 'SELECT * FROM historia_opt WHERE id_usuario = ?;';
+connection.query(sql,(err,row)=>{
+  if(err){throw err}
+  else
+  {
+    callback(null,row);
+  }
+})
+
+};
