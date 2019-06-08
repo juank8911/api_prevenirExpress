0f9ac35e926a9d50c56c3a60e031cdcc4e9da0a1
juank8911 <juancarlos_891111@hotmail.com>
2019-06-08 10:37:59 -0500

    cambios optica

--- src/routes/cateRoutes.js
+++ src/routes/cateRoutes.js
@@ -14,7 +14,9 @@ res.json(data);
 app.put('/opticausu',(req,res)=>{
   var opt = req.body;
   console.log(opt);
+
   optica.createHisto(opt,(err,resp)=>{
+
     res.json(resp);
   });
 
@@ -22,6 +24,7 @@ app.put('/opticausu',(req,res)=>{
 
 //retona la lista de los municipios segun el id del departamento
 app.get('/opticah/:id',(req,res)=>{
+  
 optica.darDatosUsu(req.params.id,(err,resp)=>{
 //console.log(resp);
 res.json(resp);
