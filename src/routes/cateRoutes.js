9a14c440bf89868dc5777f16deaf66cad144fdb8
juank8911 <juancarlos_891111@hotmail.com>
2019-06-08 11:01:36 -0500

    cate sin espacios

--- src/routes/cateRoutes.js
+++ src/routes/cateRoutes.js
@@ -13,14 +13,8 @@ res.json(data);
 
 app.put('/opticausu',(req,res)=>{
   var opt = req.body;
-<<<<<<< HEAD
   console.log(opt);
-
-=======
-  // console.log(opt);
->>>>>>> 2af98a4c3c3c7339009fc7e709b3e131573554be
   optica.createHisto(opt,(err,resp)=>{
-
     res.json(resp);
   });
 
@@ -28,7 +22,6 @@ app.put('/opticausu',(req,res)=>{
 
 //retona la lista de los municipios segun el id del departamento
 app.get('/opticah/:id',(req,res)=>{
-  
 optica.darDatosUsu(req.params.id,(err,resp)=>{
 //console.log(resp);
 res.json(resp);
